712f4c815ab80d29fba10b3a11b6fa9cff9b4e91
Ali <alifelanv@gmail.com>
2019-10-05 18:06:46 -0500

    Now scans files given as paramethers

--- mali.py
+++ mali.py
@@ -1,1 +1,9 @@
+from parser import parser
+import sys
 
+if __name__ == '__main__':
+    if len(sys.argv) is not 2:
+        print('Please send a file.')
+        raise SyntaxError('mali needs 1 file.')
+    file = open(sys.argv[1], 'r')
+    parser.parse(file.read())
