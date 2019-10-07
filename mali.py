113c12de370fc84b23038211adb23e0bfb8b140c
Ali <alifelanv@gmail.com>
2019-10-07 18:16:48 -0500

    Add program name as a variable

--- mali.py
+++ mali.py
@@ -2,12 +2,13 @@
 from parser import *
 import sys
 
+program_name = 'test'
+
 if __name__ == '__main__':
     if len(sys.argv) is not 2:
         print('Please send a file.')
         raise SyntaxError('mali needs 1 file.')
-    file = open(sys.argv[1], 'r')
+    program_name = sys.argv[1]
+    file = open(program_name, 'r')
     parser.parse(file.read())
-    pp.pprint(classes)
-
-
+    # pp.pprint(classes)
