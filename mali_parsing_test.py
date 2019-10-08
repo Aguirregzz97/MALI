75b2aa537e945fdd5c7dd528bfc2d97fc725144c
mauriciogm97 <mauriciogm97@users.noreply.github.com>
2019-10-07 20:41:04 -0500

    Finish testing automation

--- mali_parsing_test.py
+++ mali_parsing_test.py
@@ -1,12 +1,26 @@
-import glob, os
+import glob, os, io, sys
 from parser import *
 
+def parse(file):
+  text_trap = io.StringIO()
+  sys.stdout = text_trap
+  parser.parse(file.read())
+  sys.stdout = sys.__stdout__ 
+
 if __name__ == '__main__':
-    os.chdir('testfiles')
-    for file_name in glob.glob('correctos/*_test.m'):
-        file = open(file_name, 'r')
-        try:
-            parser.parse(file.read())
-            print(f'Successfull {file_name}.')
-        except SyntaxError:
-            print(f'Fail        {file_name}.')
+  os.chdir('testfiles')
+  for file_name in glob.glob('correct/*_test.m'):
+    file = open(file_name, 'r')
+    try:
+      parse(file)
+      print(f'Successfull {file_name}.')
+    except SyntaxError:
+      print(f'Fail        {file_name}.')
+
+  for file_name in glob.glob('incorrect/*_test.m'):
+    file = open(file_name, 'r')
+    try:
+      parse(file)
+      print(f'Fail        {file_name}.')
+    except SyntaxError:
+      print(f'Successfull {file_name}.')
